53e95dbffb331270845b39d0528eb8a37ac02782
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-04-27 20:46:13 -0300

    Add: Class para chamar api spotify;

---
+++ SpotifyDownloader/Comunication/Spotify.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyDownloader.Comunication
+{
+    public static class Spotify
+    {
+        internal static void DownloadPlaylist(string Link)
+        {
+
+        }
+    }
+}

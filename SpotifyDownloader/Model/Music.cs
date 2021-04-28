2f4b9d1fb2e60ec5be70ca098803b8402c0da4ba
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-04-27 21:55:09 -0300

    Add: Music model

---
+++ SpotifyDownloader/Model/Music.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyDownloader.Model
+{
+    public class Music
+    {
+        public string Name { get; set; }
+        public string Singer { get; set; }
+    }
+}
